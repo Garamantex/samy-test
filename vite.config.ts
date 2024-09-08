7e4cdb8dd4beec9a9992b2d8f571520ecc5d9826
Alexander <alexanderacosta2286@gmail.com>
2024-09-08 12:09:20 -0500

    adding tailwind and setuping prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,11 @@ export default defineConfig({
   server: {
     watch: {
       usePolling: true
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss('./tailwind.config.js')]
     }
   }
 });
